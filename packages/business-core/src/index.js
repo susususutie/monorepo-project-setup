b365d6d4a74b0ec9f6d157254141fbd720606045
sutie <susususutie@gmail.com>
2026-03-14 22:45:26 +0800

    feat(business-core): add missing index.js entry point

---
+++ packages/business-core/src/index.js
@@ -0,0 +1,6 @@
+// business-core 主入口
+export * from './api/fetch.js';
+export * from './api/posts.js';
+export { PostButton } from './components/index.js';
+export * from './constants/api.const.js';
+export { default as POST_STATUS } from './enums/POST_STATUS.js';
